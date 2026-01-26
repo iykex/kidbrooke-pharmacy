894997e4253226ca90fbb4c92d1562ed8dcb1a0a
Dennis Sekyi Opoku <denipsyp@gmail.com>
2026-01-26 04:35:27 +0000

    feat: newsletter subscription

--- components/home/newsletter.tsx
+++ components/home/newsletter.tsx
@@ -130,18 +130,14 @@ export default function NewsletterSection() {
                               <Button
                                 type="submit"
                                 form="email-form"
-                                onClick={() => {
-                                  track(
-                                    TRACKING_EVENTS.newsletterSubscribe,
-                                    "subscribed to newsletter"
-                                  );
-                                }}
                                 disabled={
-                                  formState.isLoading || formState.isSubmitting
+                                  formState.isLoading ||
+                                  formState.isSubmitting ||
+                                  !formState.isValid
                                 }
                                 className="px-6 rounded-r-xl rounded-l-none bg-primary hover:bg-primary/90 font-semibold text-white z-10"
                               >
-                                {formState.isLoading ? (
+                                {formState.isSubmitting ? (
                                   <Spinner />
                                 ) : (
                                   <>
@@ -172,7 +168,7 @@ export default function NewsletterSection() {
                             {item}
                           </span>
                         </div>
-                      )
+                      ),
                     )}
                   </div>
 
--- hooks/use-newsletter.ts
+++ hooks/use-newsletter.ts
@@ -1,24 +1,51 @@
 "use client";
+import { db } from "@/lib/firebase/firebase-client";
 import { newsletterSchema } from "@/lib/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FirebaseError } from "firebase/app";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z4 from "zod/v4";
 
 export default function useNewsletter() {
   const [isSubscribed, setIsSubscribed] = useState(false);
 
-  const { handleSubmit, control, formState } = useForm<
+  const { handleSubmit, control, formState, reset } = useForm<
     z4.infer<typeof newsletterSchema>
   >({
     resolver: zodResolver(newsletterSchema),
     defaultValues: {
       email: "",
     },
-    mode: "all",
+    mode: "onChange",
   });
 
-  function onSubmit(data: z4.infer<typeof newsletterSchema>) {}
+  async function onSubmit(data: z4.infer<typeof newsletterSchema>) {
+    try {
+      await addDoc(
+        collection(db, "tenants", "kidbrooke", "newsletter_subscribers"),
+        {
+          email: data.email.toLowerCase().trim(),
+          date: serverTimestamp(),
+        },
+      );
+      toast.success("Successfully subscribed to newsletter");
+      reset();
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        if (error.code === "permission-denied") {
+          toast.error("This email is already subscribed!");
+        } else {
+          toast.error("Subscription failed. Please try again.");
+        }
+      } else {
+        toast.error("Something went wrong. Please try again.");
+      }
+    }
+  }
+
   return {
     isSubscribed,
     setIsSubscribed,
