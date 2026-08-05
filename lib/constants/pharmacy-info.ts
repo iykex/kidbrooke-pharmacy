57626609b05f32808ca93bd399ea3d263a395886
mr•vybes <aib.quame@gmail.com>
2026-08-05 04:22:58 +0000

    feat: add verified phone number and full address to pharmacy-info

--- lib/constants/pharmacy-info.ts
+++ lib/constants/pharmacy-info.ts
@@ -1,9 +1,15 @@
 export const PHARMACY_INFO = {
   name: "Kidbrooke Pharmacy",
   slug: "kidbrooke",
-  postcode: "SE3 8AR",
-  city: "London",
-  region: "London SE",
+  phone: "020 8319 0115",
+  phoneHref: "tel:02083190115",
+  address: {
+    line1: "134 Rochester Way",
+    city: "London",
+    region: "London SE",
+    postcode: "SE3 8AR",
+    country: "United Kingdom",
+  },
   pharmadoctorUrl:
     "https://pharmadoctor.co.uk/patient/locations/london/london-se/Kidbrooke-Pharmacy-SE38AR-500",
   shortPharmadoctorUrl: "https://www.phdr.co.uk/500",
